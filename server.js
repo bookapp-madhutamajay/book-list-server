47aa5a9d16f53256615dd9371a7b68509d785cb7
tamarushin <tamarushin@gmail.com>
2018-04-14 15:25:11 -0700

    connection strings

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const PORT = process.env.PORT;
-//const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 const app = express();
-//const client = new pg.Client(DATABASE_URL);
-//const CLIENT_URL = process.env.CLIENT_URL;
+const client = new pg.Client(DATABASE_URL);
+const CLIENT_URL = process.env.CLIENT_URL;
 
-//client.connect();
-//client.on('error', err=>console.error(err));
+client.connect();
+client.on('error', err=>console.error(err));
 
 app.use(cors());
 
