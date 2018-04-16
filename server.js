2da6c94733363f4cc0c4e745faea750a57ceb1a2
Mrebb <madhu.rebbana@gmail.com>
2018-04-16 01:21:35 -0700

    get method for api/v1/books

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.get('/books',(req,res) => {
   `).then(result => res.send(result.rows))
   .catch(console.error);
 });
-    
-
+app.get('/api/v1/books',(req,res) => {
+  client.query (`
+  SELECT book_id, title, author, image_url FROM books;
+  `).then(result => res.send(result.rows))
+  .catch(console.error);
+});
+//app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
