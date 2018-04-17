6b6f7a3ead8896c2b471b96c69b3d728450c04b9
tamarushin <tamarushin@gmail.com>
2018-04-16 20:13:37 -0700

    Completed README.md

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 const app = express();
 const client = new pg.Client(DATABASE_URL);
 const CLIENT_URL = process.env.CLIENT_URL;
 
 client.connect();
-client.on('error', err=>console.error(err));
+client.on('error', err => console.error(err));
 
 app.use(cors());
 
@@ -17,6 +19,7 @@ app.get('/books',(req,res) => {
   SELECT * FROM books;
   `).then(result => res.send(result.rows))
   .catch(console.error);
+  
 });
 app.get('/api/v1/books',(req,res) => {
   client.query (`
